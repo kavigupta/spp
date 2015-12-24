589134c50a12db78c48a22d7afa8df74f775a7e9
Kavi Gupta <kavi@globalmbox.com>
2015-12-24 01:26:05 -0800

    turns out I did not need that method

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # GenericPreprocessor
 A preprocessor that is designed for simplicity and extensiblity, written in Haskell
-
-
-## Acknowledgements
-
-Most of the work given here is my own, but spp uses code from the following sources:
-
-    - http://stackoverflow.com/a/8502391/1549476
--- Setup.hs
+++ Setup.hs
@@ -13,7 +13,8 @@ main = do
     when (buildCode /= ExitSuccess) $ die "Build failed"
     hlintRun
     buildTest <- rawSystem "cabal" ["test"]
-    when (buildTest /= ExitSuccess) $ die "Build failed"
+    when (buildTest /= ExitSuccess) $ die "Tests failed"
+
 
 hlintRun :: IO ()
 hlintRun = do
@@ -24,13 +25,3 @@ hlintRun = do
 
 die :: String -> IO ()
 die msg = putStrLn msg >> exitFailure
-
-{-
-    By Stack overflow user ehird http://stackoverflow.com/users/1097181/ehird
-    At post http://stackoverflow.com/a/8502391/1549476
--}
-removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = removeFile fileName `catch` handleExists
-  where handleExists e
-          | isDoesNotExistError e = return ()
-          | otherwise = throwIO e
