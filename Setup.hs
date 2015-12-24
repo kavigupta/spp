20a19ec8c4ac47e94dc1d38af68af70fe6b905a2
Kavi Gupta <kavi@globalmbox.com>
2015-12-23 23:22:40 -0800

    Replaced setup.bash with Setup.hs that replicates the behavior

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,26 @@
+#!/usr/bin/runhaskell
 import Distribution.Simple
-main = defaultMain
+
+import System.Process
+import System.Exit
+
+import Control.Monad
+
+import Text.Regex.Posix((=~))
+
+main = do
+    buildCode <- rawSystem "cabal" ["build"]
+    when (buildCode /= ExitSuccess) $ die "Build failed"
+    hlintRun
+    buildTest <- rawSystem "cabal" ["test"]
+    when (buildTest /= ExitSuccess) $ die "Build failed"
+
+hlintRun :: IO ()
+hlintRun = do
+    (_,lintResult1, lintResult2) <- readProcessWithExitCode "hlint" ["."] ""
+    let lintResult = lintResult1 ++ lintResult2
+    when (lintResult =~ "([0-9]+)\\s+suggestions?")
+        $ die ("Hlint suggestions ==>\n" ++ lintResult)
+
+die :: String -> IO ()
+die msg = putStrLn msg >> exitFailure
