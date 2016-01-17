e1da9e7834c3e86b5bf1e9c2499b61666fc38b61
Kavi Gupta <kavi@globalmbox.com>
2016-01-17 21:26:28 +0530

    Added support to the CLI for truly optional arguments, and added output dir argument

--- src/Interface/Args.hs
+++ src/Interface/Args.hs
@@ -1,28 +1,43 @@
 module Interface.Args (
         Options,
-            srcDir, clean, directiveStart, noCleanOnErrors,
+            srcDir, outDir, clean, directiveStart, noCleanOnErrors,
         optionParser
     ) where
 
 import System.Console.ArgParser
+import System.Console.ArgParser.Parser
 
 data Options = Options {
         -- Whether or not this is a cleaning run
         clean :: Bool,
         -- The source directory to preprocess or clean
         srcDir :: String,
+        -- Where the output files should go. "" signifies to use default
+        outDir :: Maybe String,
         -- The string to start directives with
         directiveStart :: String,
         -- Clean up on errors
         noCleanOnErrors :: Bool
     }
 
---TODO Add support for specifying an output directory
 --TODO add update support
+
+nothingString :: String
+nothingString = "\x01"
 
 optionParser :: ParserSpec Options
 optionParser = Options `parsedBy`
     boolFlag "clean" `andBy`
-    reqFlag "src" `andBy`
+    reqFlag "src" `andMaybeBy`
+    optFlag nothingString "out" `andBy`
     optFlag "" "directive-start" `andBy`
     boolFlag "no-clean-on-errors"
+
+infixl 1 `andMaybeBy`
+
+andMaybeBy :: ParamSpec spec => ParserSpec (Maybe String -> b) -> spec String -> ParserSpec b
+andMaybeBy prev spec = fmap (.isNothing) prev `andBy` spec
+    where
+    isNothing x
+        | x == nothingString    = Nothing
+        | otherwise             = Just x
