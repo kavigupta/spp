e619bbf766ec02b850fee4fd28582700861025cb
Kavi Gupta <kavi@globalmbox.com>
2016-02-06 02:13:27 -0800

    renamed things to dirst

--- src/Test/Tests.hs
+++ src/Test/Tests.hs
@@ -12,7 +12,7 @@ cmdTests = [
         CmdTest "tree" 1 "--src src" "--src src --clean",
         CmdTest "error" 1 "--src src" "--src src --clean",
         CmdTest "error" 2 "--src src --no-clean-on-errors" "--src src --clean",
-        CmdTest "directive_start" 1 "--src src --directive-start \"#\" --no-clean-on-errors" "--src src --clean"
+        CmdTest "dirst" 1 "--src src --directive-start \"#\" --no-clean-on-errors" "--src src --clean"
     ]
 
 tests :: IO [Test]
