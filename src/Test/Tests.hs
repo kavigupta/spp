e817af76cb044147ba3c14db20fe8815688416b6
Kavi Gupta <kavi@globalmbox.com>
2016-02-06 23:25:26 -0800

    added replace test

--- src/Test/Tests.hs
+++ src/Test/Tests.hs
@@ -14,6 +14,7 @@ cmdTests =
         , CmdTest "error"       2 "--src src --no-clean-on-errors" "--src src --clean"
         , CmdTest "dirst_simp"  1 "--src src --directive-start '#' --no-clean-on-errors" "--src src --clean"
         , CmdTest "dirst_ignore"  1 "--src src --directive-start '#' --no-clean-on-errors" "--src src --clean"
+        , CmdTest "replace"  1 "--src src" "--src src --clean"
     ]
 
 tests :: IO [Test]
