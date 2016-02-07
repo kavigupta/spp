7544fdd7d69c2b9155735a2544a6c08b699d3dd3
Kavi Gupta <kavi@globalmbox.com>
2016-02-06 20:51:17 -0800

    dirst_test renamed to dirst_simp_test

--- src/Test/Tests.hs
+++ src/Test/Tests.hs
@@ -6,13 +6,13 @@ import Test.CmdTests
 import Distribution.TestSuite
 
 cmdTests :: [CmdTest]
-cmdTests = [
-        CmdTest "basic" 1 "--src src" "--src src --clean",
-        CmdTest "basic" 2 "--src src --out out" "--src src --out out --clean",
-        CmdTest "tree" 1 "--src src" "--src src --clean",
-        CmdTest "error" 1 "--src src" "--src src --clean",
-        CmdTest "error" 2 "--src src --no-clean-on-errors" "--src src --clean",
-        CmdTest "dirst" 1 "--src src --directive-start \"#\" --no-clean-on-errors" "--src src --clean"
+cmdTests =
+        [ CmdTest "basic"       1 "--src src" "--src src --clean"
+        , CmdTest "basic"       2 "--src src --out out" "--src src --out out --clean"
+        , CmdTest "tree"        1 "--src src" "--src src --clean"
+        , CmdTest "error"       1 "--src src" "--src src --clean"
+        , CmdTest "error"       2 "--src src --no-clean-on-errors" "--src src --clean"
+        , CmdTest "dirst_simp"  1 "--src src --directive-start '#' --no-clean-on-errors" "--src src --clean"
     ]
 
 tests :: IO [Test]
