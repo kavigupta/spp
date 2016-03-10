6e55e78b3adc5b7678d9ddf38a7e3315256b9ace
Kavi Gupta <kavi@globalmbox.com>
2016-03-10 13:11:13 -0800

    Now passing in more state about current and dependency chaining into the preprocess function

--- src/Preprocessor.hs
+++ src/Preprocessor.hs
@@ -4,7 +4,6 @@
 module Preprocessor (preprocessAll) where
 
 import Control.Applicative hiding ((<|>))
-import Control.Monad
 
 import Control.Exception(catch, evaluate)
 
@@ -17,17 +16,28 @@ import Directive.Parser
 import Interface.Errors
 
 preprocessAll :: SPPOpts -> [BackedUpFile] -> IO [Either SPPError ()]
-preprocessAll opts buf = forM buf $ preprocess opts
+preprocessAll _ [] = return []
+preprocessAll opts (x:xs) = 
+    do
+        c <- preprocess opts ToPreprocess {current=x,future=xs, depChain=[]}
+        rest <- preprocessAll opts xs
+        return $ c:rest
+
+data ToPreprocess = ToPreprocess {
+          current :: BackedUpFile
+        , future :: [BackedUpFile]
+        , depChain :: [BackedUpFile]
+    }
 
 {-
 An IO Action for either processing a file or producing an error without doing anything
 -}
-preprocess :: SPPOpts -> BackedUpFile -> IO (Either SPPError ())
+preprocess :: SPPOpts -> ToPreprocess -> IO (Either SPPError ())
 preprocess sppopts buFile =
         do
-            output sppopts Verbose $ "Output File = " ++ outputFile buFile ++ "\n"
+            output sppopts Verbose $ "Output File = " ++ outputFile (current buFile) ++ "\n"
             -- Ignore the possibility of error at this line.
-            mcontents <- fmap Right (readFile (backupFile buFile) >>= evaluate)
+            mcontents <- fmap Right (readFile (backupFile . current $ buFile) >>= evaluate)
                     `catch` (return . Left :: IOError -> IO (Either IOError String))
             output sppopts Debug $ "Received output = "++ show mcontents ++ "\n"
             case mcontents of
@@ -35,34 +45,34 @@ preprocess sppopts buFile =
                     | isBinaryError err
                         -> return $ Right ()
                     | otherwise
-                        -> return $ Left $ sppError ReadIOError (sourceFile buFile) Nothing err
+                        -> return $ Left $ sppError ReadIOError (sourceFile . current $ buFile) Nothing err
                 (Right contents) -> do
                     -- There should be no error at this line given that process should throw no error
                     outp <- process sppopts buFile contents
                     case outp of
                         SPPFailure err -> return $ Left err
-                        SPPSuccess outputValue -> Right <$> writeFile (outputFile buFile) (fContents outputValue)
+                        SPPSuccess outputValue -> Right <$> writeFile (outputFile . current $ buFile) (fContents outputValue)
 
 isBinaryError :: IOError -> Bool
 isBinaryError err = "hGetContents: invalid argument (invalid byte sequence)" `isInfixOf` show err
 
-performCommands :: SPPOpts -> BackedUpFile -> Directives Command -> IO PreprocessorResult
+performCommands :: SPPOpts -> ToPreprocess -> Directives Command -> IO PreprocessorResult
 performCommands opts buf (Directives header commands rest) = do
         output opts Debug $ "Directives =" ++ show (Directives header commands rest) ++ "\n"
-        result <- performAll actions $ SPPState {fContents=rest, dependencyChain=[], possibleFiles=[]}
+        result <- performAll actions $ SPPState {fContents=rest, dependencyChain=depChain buf, possibleFiles=future buf}
         output opts Debug $ "Result = " ++ show result ++ "\n"
         return $ mapOverSuccess (mapOverContents (header ++)) result
-    where actions = map (getCommand buf) commands
+    where actions = map (getCommand $ current buf) commands
 
 {-
 Creates an IO instance for preprocessing a series of lines.
 -}
-process :: SPPOpts -> BackedUpFile -> String -> IO PreprocessorResult
+process :: SPPOpts -> ToPreprocess -> String -> IO PreprocessorResult
 process sppopts buf str
         = either (return . SPPFailure) id result
     where
     result :: Either SPPError (IO PreprocessorResult)
-    result = performCommands sppopts buf <$> parseDirectives (directiveStart sppopts) (sourceFile buf) str
+    result = performCommands sppopts buf <$> parseDirectives (directiveStart sppopts) (sourceFile . current $ buf) str
 {-
     Perform all the actions in the given list of actions.
     If any of the values are `Left` errors, the entire result is an error.
